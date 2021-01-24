2a3adaad2ce4750c25507f2e1595679230c9b5c0
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2021-01-24 09:23:26 +0100

    Add redirects file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { user } from './reducer/user';
 
-import { LandingPage } from './pages/LandingPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { LogInPage } from './pages/LogInPage';
+import { BabyProfileForm } from './pages/BabyProfileForm';
+import { Dashboard } from './pages/Dashboard';
 
 const reducer = combineReducers({ user: user.reducer });
 const store = configureStore({ reducer });
@@ -12,7 +16,25 @@ const store = configureStore({ reducer });
 export const App = () => {
   return (
     <Provider store={store}>
-      <LandingPage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <SignUpPage />
+          </Route>
+
+          <Route path="/login">
+            <LogInPage />
+          </Route>
+
+          <Route path="/create-profile">
+            <BabyProfileForm />
+          </Route>
+
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 };
